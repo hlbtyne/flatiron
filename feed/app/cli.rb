7511c5b77923e04394f1d2553f95973e24b826d2
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-16 15:42:12 +0100

    MVP working: search term validation

--- feed/app/cli.rb
+++ feed/app/cli.rb
@@ -4,74 +4,71 @@ class CLI
    @prompt = TTY::Prompt.new
  end
 
- # ask_name
- #  prompts user for name
-    # adds or creates user in db
+ #  asks user for name
+ #  finds or creates user in db
   def find_or_create_user
     puts "Welcome to Feed!"
     name = @prompt.ask("Whats your name? ")
     @user = User.find_or_create_by(name: name)
   end
 
+#   greets user
   def greet
       puts "Hi #{@user.name},"
   end
 
-  #   prompt user for search term get search term
-  def search_recipes
+#   prompts user for search term and gets search term
+  def search_ingredient
     @ing = @prompt.ask("Please enter ingredient name to search recipes: ")
   end
 
-  # def another_search
-  #   @ing = @prompt.ask("Please enter ingredient name to search recipes: ")
-  # end
+#   selects all recipe ingredients matching search term
+  def recipe_ingredients
+    RecipeIngredient.all.select { |ri| ri.ingredient.name == @ing }
+  end
 
-
-  #   return relevant recipes based on search term
+  #   returns relevant recipes based on search term
+  #   gives error message if search term has no matches
   def display_search_results
-    rec_ing = RecipeIngredient.all.select { |ri| ri.ingredient.name == @ing }
-    # while !rec_ing
-    #   puts "Ingredient not found, please try again."
-    #   search_again
-    # end
+    rec_ing = recipe_ingredients
+    while rec_ing.length == 0
+      puts "Sorry, that ingredient's not available."
+      search_ingredient
+      rec_ing = recipe_ingredients
+    end
     @rec = rec_ing.map { |ri| ri.recipe }
     @rec_titles = @rec.map { |r| r.title }
-  end
-
-  #   if no ingredient found, prompts again
-
-  # select a recipe and display content
-  def select_and_show_recipe
-    selection = @prompt.select("Please select a recipe:", (@rec_titles))
-    selected_rec = @rec.find { |recipe| recipe.title == selection }
-    puts selected_rec.content
-    # binding.pry
-    # "eof"
-  end
-
-  def search_again
-    search_recipes
-    display_search_results
     select_and_show_recipe
-    search_again?
   end
 
+  #   prompts user to select a recipe fo list
+  #   displays content of selected recipe
+    def select_and_show_recipe
+      selection = @prompt.select("Please select a recipe:", (@rec_titles))
+      selected_rec = @rec.find { |recipe| recipe.title == selection }
+      puts selected_rec.content
+    end
+
+#   asks user if they want to search again
   def search_again?
     answer = @prompt.yes?("Would you like to search another ingredient?")
     if answer
-      search_again
+      find_recipe
     else
       puts "Thanks for using Feed. See you next time!"
     end
   end
 
+  def find_recipe
+    search_ingredient
+    display_search_results
+    search_again?
+  end
+
   def run
     find_or_create_user
     greet
-    search_recipes
-    display_search_results
-    select_and_show_recipe
-    search_again?
+    find_recipe
   end
 
 end
