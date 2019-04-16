8e49481cb1f0ef5a8a9ea49712e5e58a7969a5a3
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-16 14:13:44 +0100

    Search again? loop added

--- feed/app/cli.rb
+++ feed/app/cli.rb
@@ -13,23 +13,34 @@ class CLI
     @user = User.find_or_create_by(name: name)
   end
 
-  # Search
-  #   prompts user for search term
-  #   returns top 3 recipes
-  #   if no ingredient found, prompts again
-  def search_recipes
-    puts "Hi #{@user.name},"
-    ing = @prompt.ask("Please enter ingredient name to search recipes: ")
-    
-    rec_ing = RecipeIngredient.all.select { |ri| ri.ingredient.name == ing }
-    @rec = rec_ing.map { |ri| ri.recipe }
-    @rec_titles = @rec.map { |r| r.title }
-    # binding.pry
-    # 'eof'
+  def greet
+      puts "Hi #{@user.name},"
   end
 
-  # select_recipe
-  #   display full recipe
+  #   prompt user for search term get search term
+  def search_recipes
+    @ing = @prompt.ask("Please enter ingredient name to search recipes: ")
+  end
+
+  # def another_search
+  #   @ing = @prompt.ask("Please enter ingredient name to search recipes: ")
+  # end
+
+
+  #   return relevant recipes based on search term
+  def display_search_results
+    rec_ing = RecipeIngredient.all.select { |ri| ri.ingredient.name == @ing }
+    # while !rec_ing
+    #   puts "Ingredient not found, please try again."
+    #   search_again
+    # end
+    @rec = rec_ing.map { |ri| ri.recipe }
+    @rec_titles = @rec.map { |r| r.title }
+  end
+
+  #   if no ingredient found, prompts again
+
+  # select a recipe and display content
   def select_and_show_recipe
     selection = @prompt.select("Please select a recipe:", (@rec_titles))
     selected_rec = @rec.find { |recipe| recipe.title == selection }
@@ -38,10 +49,29 @@ class CLI
     # "eof"
   end
 
+  def search_again
+    search_recipes
+    display_search_results
+    select_and_show_recipe
+    search_again?
+  end
+
+  def search_again?
+    answer = @prompt.yes?("Would you like to search another ingredient?")
+    if answer
+      search_again
+    else
+      puts "Thanks for using Feed. See you next time!"
+    end
+  end
+
   def run
     find_or_create_user
+    greet
     search_recipes
+    display_search_results
     select_and_show_recipe
+    search_again?
   end
 
 end
