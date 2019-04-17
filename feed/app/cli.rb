2130b32eae0593e692ce0378eacd0ea494a80b7b
hlbtyne <harriet.ballantyne@gmail.com>
2019-04-17 14:38:18 +0100

    Bug fixes, delete function added

--- feed/app/cli.rb
+++ feed/app/cli.rb
@@ -9,6 +9,10 @@ class CLI
   def find_or_create_user
     puts "Welcome to Feed!"
     name = @prompt.ask("Whats your name? ")
+    while name == nil
+      puts "Please enter a valid name."
+      name = @prompt.ask("Whats your name? ")
+    end
     @user = User.find_or_create_by(name: name)
   end
 
@@ -20,20 +24,52 @@ class CLI
     if ans == "View recipe book"
       user_recipes = @user.view_recipe_book
       select_recipe_from_book(user_recipes)
+    else
+      find_recipe
     end
   end
 
   #   prompts user to select a recipe from recipe book
   #   displays content of selected recipe
   def select_recipe_from_book(recipes)
-    selection = @prompt.select("Please select a recipe:", (recipes))
-    @selected_rec = @user.recipes.find { |recipe| recipe.title == selection }
-    puts @selected_rec.content
+    if recipes.length == 0
+      puts "You haven't saved any recipes."
+      choose_option
+    else
+      options = [recipes, "Delete a recipe", "Back"]
+      selection = @prompt.select("Your recipe book", (options))
+      if selection == "Delete a recipe"
+        select_recipe_to_delete(recipes)
+      elsif selection == "Back"
+        choose_option
+      else
+        @selected_rec = @user.recipes.find { |recipe| recipe.title == selection }
+        puts @selected_rec.content
+        choose_option
+      end
+    end
+  end
+
+  def select_recipe_to_delete(recipes)
+    delete_options = [recipes, "Back"]
+    ans = @prompt.select("Select a recipe to delete", (delete_options))
+    if ans == "Back"
+      select_recipe_from_book(@user.view_recipe_book)
+    else
+      delete_me = @user.recipes.find { |recipe| recipe.title == ans }
+      @user.delete_recipe_from_book(delete_me)
+      select_recipe_from_book(@user.view_recipe_book)
+    end
   end
 
 #   prompts user for search term and gets search term
   def search_ingredient
-    @ing = @prompt.ask("Please enter ingredient name to search recipes: ")
+    search_term = @prompt.ask("Please enter ingredient name to search recipes: ")
+    while search_term == nil
+      puts "Please enter a valid search term."
+      search_term = @prompt.ask("Please enter ingredient name to search recipes: ")
+    end
+    @ing = search_term.downcase
   end
 
 #   selects all recipe ingredients matching search term
@@ -61,28 +97,30 @@ class CLI
 
   #   prompts user to select a recipe from list of search results
   #   displays content of selected recipe
-    def select_recipe_from_search(recipes)
-      selection = @prompt.select("Please select a recipe:", (recipes))
-      @selected_rec = recipe_instances.find { |recipe| recipe.title == selection }
-      puts @selected_rec.content
-    end
+  def select_recipe_from_search(recipes)
+    selection = @prompt.select("Please select a recipe:", (recipes))
+    @selected_rec = recipe_instances.find { |recipe| recipe.title == selection }
+    puts @selected_rec.content
+  end
 
 #   Ask usr if they want to save
 #   Create new user recipe if yes
-    def save?
-    ans = @prompt.yes?("Would you like to save #{@selected_rec.title} recipe to your recipe book?")
-    if ans
+  def save?
+    ans = @prompt.select("Would you like to save this #{@selected_rec.title} recipe to your recipe book?", %w(Yes No))
+    if ans == "Yes"
       @user.save_recipe(@selected_rec)
-      search_again?
-    else
-      search_again?
     end
+    choose_option
   end
 
 #   asks user if they want to search again
-  def search_again?
-    answer = @prompt.yes?("Would you like to search another ingredient?")
-    if answer
+  def choose_option
+    options = ["View recipe book", "Search by ingredient", "Quit app"]
+    answer = @prompt.select("Choose an option:", (options))
+    if answer == "View recipe book"
+      user_recipes = @user.view_recipe_book
+      select_recipe_from_book(user_recipes)
+    elsif answer == "Search by ingredient"
       find_recipe
     else
       puts "Thanks for using Feed. See you next time!"
@@ -98,7 +136,7 @@ class CLI
   def run
     find_or_create_user
     greet
-    find_recipe
+    # find_recipe
   end
 
 end
--- feed/app/models/user.rb
+++ feed/app/models/user.rb
@@ -2,13 +2,20 @@ class User < ActiveRecord::Base
   has_many :user_recipes
   has_many :recipes, through: :user_recipes
 
+  validates :name, presence: true
+
+#   Saves a recipe to the user's recipe book by creating a user recipe instance
   def save_recipe(recipe)
     UserRecipe.find_or_create_by(user_id: self.id, recipe_id: recipe.id)
   end
 
   def view_recipe_book
-     user_rec = self.user_recipes
-     recipes = user_rec.map { |r| r.recipe }
-     my_rec_titles = recipes.map { |r| r.title }
+    my_updated_userrecipes = UserRecipe.all.select{ |ur| ur.user == self}
+    my_updated_recipes = my_updated_userrecipes.map{|ur| ur.recipe.title}
+    my_updated_recipes
+  end
+
+  def delete_recipe_from_book(recipe)
+    self.recipes.destroy(recipe)
   end
 end
